1db9bf95815148505bfc402b5ba7544a3ad0fdc0
Zwolfy <14087614+zwolfy@users.noreply.github.com>
2021-07-12 14:10:46 -0400

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Injectable()
+
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly life goal']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal : any) {
+    this.goals.next(goal);
+  }
+
+}
